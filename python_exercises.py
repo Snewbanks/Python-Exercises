d50c02f3adc34551f618c0621151695552750857
Snewbanks <Shane.Newbanks@gmail.com>
2021-11-02 09:40:50 -0500

    first commit

---
+++ python_exercises.py
@@ -0,0 +1,18 @@
+# Exercise 1
+number1 = int(input("Enter First Number"))
+number2 = int(input("Enter Second Number"))
+
+print("add numbers together")
+add = input("+")
+
+if add = "+":
+    equals = number1 + number2
+    
+print(number1, add, number2, equals)
+
+
+
+# Exercise 2
+def pyramid(n):
+    s = n - 1
+    for i in range (0, n):
